74958ac248a69d245a2add17a80da45713e580ff
Tomas Carrasquero <cardenalthompson@hotmail.com>
2024-04-19 20:31:14 -0300

    Iniciando Proyecto, creacion de clases

---
+++ Tp_Winform_Carrasquero_Hoffman_/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tp_Winform_Carrasquero_Hoffman_
+{
+    public class Articulo
+    {
+        public int id { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string imagen { get; set; }
+        public Marca marca { get; set; }
+        public Categoria categoria { get; set; }
+        public decimal precio { get; set; }
+    }
+}
---
+++ Tp_Winform_Carrasquero_Hoffman_/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Tp_Winform_Carrasquero_Hoffman_
+{
+    partial class MenuPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelMenuPrincipal = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.labelMenuPrincipal);
+            this.panel1.Location = new System.Drawing.Point(3, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1111, 132);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelMenuPrincipal
+            // 
+            this.labelMenuPrincipal.AutoSize = true;
+            this.labelMenuPrincipal.Font = new System.Drawing.Font("Arial", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMenuPrincipal.Location = new System.Drawing.Point(337, 28);
+            this.labelMenuPrincipal.Name = "labelMenuPrincipal";
+            this.labelMenuPrincipal.Size = new System.Drawing.Size(358, 55);
+            this.labelMenuPrincipal.TabIndex = 0;
+            this.labelMenuPrincipal.Text = "Menú Principal";
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 629);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MenuPrincipal";
+            this.Text = "Principal";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelMenuPrincipal;
+    }
+}
+
