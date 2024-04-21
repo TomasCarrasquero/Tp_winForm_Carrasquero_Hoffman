25a84da5ca3f9f8497d66488cc9f4998ea2fd110
Tomas Carrasquero <cardenalthompson@hotmail.com>
2024-04-21 20:30:56 -0300

    pantalla principal

--- Tp_Winform_Carrasquero_Hoffman_/Form1.Designer.cs
+++ Tp_Winform_Carrasquero_Hoffman_/Form1.Designer.cs
@@ -30,7 +30,16 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.labelMenuPrincipal = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDetalles = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.pbxLista = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLista)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -52,18 +61,91 @@
             this.labelMenuPrincipal.TabIndex = 0;
             this.labelMenuPrincipal.Text = "Menú Principal";
             // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(23, 395);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(172, 44);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Agregar articulo";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDetalles
+            // 
+            this.btnDetalles.Location = new System.Drawing.Point(266, 395);
+            this.btnDetalles.Name = "btnDetalles";
+            this.btnDetalles.Size = new System.Drawing.Size(172, 44);
+            this.btnDetalles.TabIndex = 2;
+            this.btnDetalles.Text = "Detalles";
+            this.btnDetalles.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(499, 395);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(172, 44);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscar.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.txtBuscar.Location = new System.Drawing.Point(23, 462);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(415, 30);
+            this.txtBuscar.TabIndex = 4;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(462, 462);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 38);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Location = new System.Drawing.Point(12, 183);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.RowHeadersWidth = 51;
+            this.dgvLista.RowTemplate.Height = 24;
+            this.dgvLista.Size = new System.Drawing.Size(686, 187);
+            this.dgvLista.TabIndex = 6;
+            // 
+            // pbxLista
+            // 
+            this.pbxLista.Location = new System.Drawing.Point(738, 178);
+            this.pbxLista.Name = "pbxLista";
+            this.pbxLista.Size = new System.Drawing.Size(350, 314);
+            this.pbxLista.TabIndex = 7;
+            this.pbxLista.TabStop = false;
+            // 
             // MenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1112, 629);
+            this.ClientSize = new System.Drawing.Size(1112, 511);
+            this.Controls.Add(this.pbxLista);
+            this.Controls.Add(this.dgvLista);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnDetalles);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MenuPrincipal";
             this.Text = "Principal";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLista)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +153,13 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label labelMenuPrincipal;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDetalles;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.PictureBox pbxLista;
     }
 }
 
